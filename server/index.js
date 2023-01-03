948e1186ffa78da93bf93c7001ebe4b143839d23
Kaizin <88437987+kaizinbr@users.noreply.github.com>
2023-01-03 15:05:59 -0300

    a

--- server/index.js
+++ server/index.js
@@ -3,17 +3,8 @@ import 'dotenv/config'
 import express from 'express';
 import morgan from 'morgan';
 
-import curriculo from './models/curriculum.js';
-import user from './models/usuario.js';
-
-import path from 'path';
-import {fileURLToPath} from 'url';
-import { jsPDF } from "jspdf";
-
 import router from './routes.js'; 
 
-// Default export is a4 paper, portrait, using millimeters for units
-const doc = new jsPDF();
 
 const app = express();
 app.use(express.json())
